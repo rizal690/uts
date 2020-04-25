3e817fbf9c3710b7d93a193d86acaa19d4abc41b
rizal690 <rizalhendri935@outlook.com>
2020-04-25 14:04:08 +0700

    berhasil menampilkan semua data buku

--- controller.js
+++ controller.js
@@ -7,3 +7,14 @@ exports.index = function(req,res){
     response.ok("Aplikasi REST API ku berjalan!", res)
 };
 
+//menampilkan semua data buku
+exports.tampilsemuabuku = function(req, res){
+    connection.query('SELECT * FROM buku', function(error, rows, fields){
+        if(error){
+            console.log(error);
+
+        } else {
+            response.ok(rows, res)
+        }
+    });
+};
